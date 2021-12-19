5bb9ea8ec7bf9fa2c44973f7b4fe6ee307b58312
urabodnargood <urabodnargood@gmail.com>
2021-12-20 00:06:39 +0200

    fix lab 8

--- Project_from_3/app/__init__.py
+++ Project_from_3/app/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
+bcrypt = Bcrypt(app)
 login_manager.login_view='login'
 login_manager.login_message_category='info'
 
